884b3126f3c1af6a5aec954aa7eab2f5ac5a92b7
Huy Nguyen <nhathuy120796@gmail.com>
2023-09-24 22:55:19 +0700

    daily update sunday

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
+import { useEffect, useState } from "react";
 import BodyContent from "./components/Body";
 import Header from "./components/Header";
 import "./style/style.css"
+import { DUMMY_BOOK } from "./data/enum";
 
 function App() {
+  const [bookList, setBookList] = useState([]);
+  const [isOpenAddmodal, setIsOpenAddModal] = useState(false);
+  const [isOpenDeleteModal, setIsOpenDeleteModal] = useState(false);
+  useEffect(() => {
+    const getDatabase = localStorage.getItem("bookList");
+    if (!getDatabase) {
+      localStorage.setItem("bookList", JSON.stringify(DUMMY_BOOK))
+    } else {
+      setBookList(JSON.parse(getDatabase))
+    }
+  }, []);
   return (
     <>
       <Header />
-      <BodyContent />
+      <BodyContent
+        bookList={bookList}
+        isOpenAddmodal={isOpenAddmodal}
+        isOpenDeleteModal={isOpenDeleteModal}
+      />
     </>
   );
 }
