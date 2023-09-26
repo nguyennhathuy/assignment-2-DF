b3eb02e32726dcf3d4db8b9bc099402099398e0d
Huy Nguyen <nhathuy120796@gmail.com>
2023-09-26 17:10:07 +0700

    daily commit 1

--- src/data/enum.js
+++ src/data/enum.js
@@ -1,15 +1,18 @@
 export const DUMMY_BOOK = [
     {
+        id: '1',
         name: "Refactoring",
         author: "Martin Fowler",
         topic: "Programming"
     },
     {
+        id: '2',
         name: "Designing Data-Intensive Applications",
         author: "Martin Kleppmann",
         topic: "Database"
     },
     {
+        id: '3',
         name: "The Phoenix Project",
         author: "Gene Kim",
         topic: "DevOps"
